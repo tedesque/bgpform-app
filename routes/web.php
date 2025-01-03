ec5d781f21b2ff7ddebcfe4a8e0e39cbcfc11720
Samuel Espíndola Tedesque <samuel.tedesque@horizontag.com>
2025-01-03 15:42:15 +0000

    redireciona a rota principal para '/assign'

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+
+// Route::view('dashboard', 'dashboard')
+//     ->middleware(['auth', 'verified'])
+//     ->name('dashboard');
 
 Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
+
+Route::get('/', function() {
+    return redirect('/assign');
+});
 
 Route::get('/assign/{token}', \App\Livewire\ClientAssign::class);
 Route::get('/assign', \App\Livewire\InsertToken::class);
